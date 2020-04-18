5ff0cd115ff9d4b9401a4c85671582102acd2ddd
Mubashir Reshamwala <mubashir.reshamwala@speedboxapp.com>
2020-04-18 14:42:28 +0530

    added validation

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
+import * as yup from 'yup';
+import Form from 'react-formal';
+const modelSchema = yup.object({
+  email: yup
+    .string()
+    .email('Invalid email ID')
+    .required('Email ID is required'),
+  password: yup.string().min(8),
+});
 export default () => {
+  const initialState = {
+    email: '',
+    password: '',
+  };
+  const [model, setModel] = useState(initialState);
   return (
     <>
       <Navbar />
       <div className="jumbotron">
         <div className="card">
-          <div className="card-header">
-            <h2>Login</h2>
-          </div>
+          <div className="card-header">{/* <h2>Login</h2> */}</div>
           <div className="card-body">
-            <input placeholder="username" type="text" className="form-control"></input>
+            <Form className="w-100" schema={modelSchema} value={model} onSubmit={e => console.log(e)} onChange={model => setModel(model)}>
+              <fieldset className="w-100">
+                <legend className="text-center">Login</legend>
+
+                <Form.Field className="form-control mt-3 w-100" name="email" placeholder="Your email ID" />
+                <Form.Message className="pl-1 text-danger" for="email" />
+
+                <Form.Field className="form-control mt-3 w-100" name="password" placeholder="Password" type="password" />
+                <Form.Message className="pl-1 text-danger" for="password" />
+                <Form.Submit className="btn btn-primary mt-3 w-100" type="submit">
+                  Submit
+                </Form.Submit>
+              </fieldset>
+            </Form>
+            {/* <input placeholder="username" type="text" className="form-control"></input>
             <input placeholder="password" type="text" className="form-control mt-3"></input>
             <Link className="btn btn-primary mt-3 px-5 py-2" to="/feed">
               Login
-            </Link>
+            </Link> */}
           </div>
         </div>
       </div>
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,26 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import './Login.css'
 import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
+import * as yup from 'yup';
+import Form from 'react-formal';
+const regex = /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/;
+const modelSchema = yup.object({
+  name: yup.string().required('Name is required'),
+  email: yup
+    .string()
+    .email('Invalid email ID')
+    .required('Email ID is required'),
+  mobile: yup
+    .number()
+    .required('Name is required')
+    .min(1000, 'Mobile number too short')
+    .max(9999999999, 'Mobile number too long')
+    .round('trunc'),
+  password: yup.string().matches(
+    regex,
+    `
+    Password must be atleast 8 characters long and contain uppercase and lowercase letters, digits and special characters`
+  ),
+  confirmPassword: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match'),
+});
 export default () => {
+  const initialState = {
+    name: '',
+    email: '',
+    mobile: '',
+    password: '',
+    confirmPassword: '',
+  };
+  const [model, setModel] = useState(initialState);
   return (
     <>
       <Navbar />
       <div className="jumbotron">
         <div className="card">
-          <div className="card-header">
-            <h2>Sign Up</h2>
-          </div>
+          <div className="card-header">{/* <h2>Sign Up</h2> */}</div>
           <div className="card-body">
-            <input placeholder="Name" type="text" className="form-control"></input>
-            <input placeholder="Username" type="text" className="form-control mt-3"></input>
-            <input placeholder="Mobile" type="number" className="form-control mt-3"></input>
-            <input placeholder="Email" type="email" className="form-control mt-3"></input>
-            <input placeholder="Password" type="text" className="form-control mt-3"></input>
-            <input placeholder="Confirm password" type="text" className="form-control mt-3"></input>
-            <Link className="btn btn-primary mt-3 px-5 py-2" to="/login">
-              Register
-            </Link>
+            <Form className="w-100" schema={modelSchema} value={model} onSubmit={e => console.log(e)} onChange={model => setModel(model)}>
+              <fieldset className="w-100">
+                <legend className="text-center">Signup</legend>
+
+                <Form.Field className="form-control mt-3 w-100" name="name" placeholder="Your name" />
+                <Form.Message className="pl-1 text-danger" for="name" />
+
+                <Form.Field className="form-control mt-3 w-100" name="email" placeholder="Your email ID" />
+                <Form.Message className="pl-1 text-danger" for="email" />
+
+                <Form.Field className="form-control mt-3 w-100" name="mobile" placeholder="Your mobile number" />
+                <Form.Message className="pl-1 text-danger" for="mobile" />
+
+                <Form.Field className="form-control mt-3 w-100" name="password" placeholder="Password" type="password" />
+                <Form.Message className="pl-1 text-danger" for="password" />
+
+                <Form.Field className="form-control mt-3 w-100" name="confirmPassword" placeholder="Re-enter password" type="password" />
+                <Form.Message className="pl-1 text-danger" for="confirmPassword" />
+                <Form.Submit className="btn btn-primary mt-3 w-100" type="submit">
+                  Submit
+                </Form.Submit>
+              </fieldset>
+            </Form>
           </div>
         </div>
       </div>
